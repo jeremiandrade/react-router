243457e6638ce483f75da0fd0f848886c4a6ce65
jeremiandrade <jeremiandrade04@gmail.com>
2026-05-16 20:31:52 +0200

    add fetch

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
-          <Route path="/" element={<Homepage />} />
-          <Route path="/" element={<Homepage />} />
+          <Route path="/prodotti" element={<h1>prodotti</h1>} />
+          <Route path="/chi siamo" element={<h1>chi Siamo</h1>} />
         </Routes>
       </BrowserRouter>
 
--- src/assets/pages/HomePage.jsx
+++ src/assets/pages/HomePage.jsx
@@ -1,22 +1,37 @@
 
+import { useState, useEffect } from "react"
 export default function Homepage() {
 
+
+
+    const api_url = ('https://fakestoreapi.com/products')
+    const [Prodotti, setProdotti] = useState([])
+
+    useEffect(() => {
+
+        fetch(api_url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setProdotti(data)
+            })
+    }, [])
     return (
         <>
             <header>
+
                 <nav className="nav-bar">
-                    <ul class="nav justify-content-center">
-                        <li>
-                            <a class="nav-link active" aria-current="page" href="#">Jr</a>
+
+                    <ul className="nav justify-content-center">
+                        <li><img src="" alt="" /></li>
+                        <li className="link">
+                            <a className=" nav-item nav-link active" aria-current="page" href="/homepage">Homepage</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Homepage</a>
+                        <li className="link">
+                            <a className="nav-item nav-link" href="/prodotti">Prodotti</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Prodotti</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Chi Siamo</a>
+                        <li className="link">
+                            <a className="nav-item nav-link" href="/Chi-siamo">Chi Siamo</a>
                         </li>
                     </ul>
                 </nav>
@@ -26,46 +41,23 @@ export default function Homepage() {
             {/* main */}
             <main>
                 <div className="d-flex m-5">
-
                     <div className="row row-cols-1 row-cols-md-2 g-4">
-                        <div className="col">
-                            <div className="card">
-                                <img src="" alt="" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        {Prodotti.map(item => (
+                            <div className="col" key={item.id}>
+                                <div className="card">
+                                    <img src="" alt="" />
+                                    <div className="card-body">
+                                        <h5 className="card-title">Card title</h5>
+                                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <img src="" alt="" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <img src="" alt="" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="card">
-                                <img src="" alt="" />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
 
+                        ))
+
+                        }
+
+                    </div>
                 </div>
 
             </main>
@@ -74,56 +66,42 @@ export default function Homepage() {
             <footer>
                 <div className="d-flex">
 
-                    <p>serve aiuto?</p>
                     <ul>
-                        <li><p>serve aiuto?</p></li>
-                        <li>faq</li>
-                        <li>faq</li>
-                        <li>faq</li>
-                        <li>faq</li>
+                        <li><p>POSSIAMO AIUTARTI</p></li>
+                        <li><a href="/prodotti">Prodotti</a></li>
+                        <li><a href="/chi siamo">Chi Siamo</a></li>
+                        <li><a href="#">Contatti</a></li>
+
                     </ul>
+
                     <ul>
-                        <p>serve aiuto?</p>
+                        <p>I NOSTRI SOCIAL</p>
                         <ul>
-                            <li>faq</li>
-                            <li>faq</li>
-                            <li>faq</li>
-                            <li>faq</li>
+                            <li><i className="bi bi-whatsapp"></i></li>
+                            <li><i className="bi bi-instagram"></i></li>
+                            <li><i className="bi bi-facebook"></i></li>
+                            <li><i className="bi bi-twitter-x"></i></li>
+                            <li><i className="bi bi-youtube"></i></li>
                         </ul>
                     </ul>
                     <ul>
-                        <p>serve aiuto?</p>
+                        <p>RESTA CONNESSO</p>
                         <ul>
-                            <li>faq</li>
-                            <li>faq</li>
-                            <li>faq</li>
-                            <li>faq</li>
-                        </ul>
-                    </ul>
-                    <ul>
-                        <p>serve aiuto?</p>
-                        <ul>
-                            <li>faq</li>
-                            <li>faq</li>
-                            <li>faq</li>
-                            <li>faq</li>
+                            <li>Copyright © 2026 ... Inc. Tutti i diritti riservati.</li>
+
                         </ul>
                     </ul>
                 </div>
 
-                <div className="card">
+                {/* <div className="card">
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 
                     </div>
                     <img src="Name " alt="" />
-                </div>
-
-
-
-
-            </footer>
+                </div> */}
+            </footer >
 
 
 
